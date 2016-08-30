6b93a781441badd725786d7d7762188d1e30cd65
Maciek Slusarczyk <mslus@op.pl>
2016-08-30 23:33:38 +0200

    1_6 and 1_7

---
+++ arraysandstrings/compression1_6.cc
@@ -0,0 +1,50 @@
+/*
+ * compression1_6.cc
+ *
+ *  Created on: Aug 30, 2016
+ *      Author: maciek
+ */
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+
+using namespace std;
+
+
+string compress(string s){
+	int i = 0;
+	int k;
+	string result;
+	stringstream ss;
+
+	while(i < s.length()){
+		k = i;
+		while(s[k] == s[i]) k++;
+		ss << s[i] << (k-i);
+		i=k;
+	}
+
+	result = ss.str();
+
+	if(result.length() < s.length())
+		return result;
+	else
+		return s;
+}
+
+int main(){
+
+	cout << compress("aabccccaddd") << endl;
+	cout << compress("aabccZZZdd") << endl;
+	cout << compress("abcd") << endl;
+	cout << compress("zxxcss") << endl;
+	cout << compress("aabccccaaaaaBBBbbb") << endl;
+
+	return 0;
+}
+
+
+
